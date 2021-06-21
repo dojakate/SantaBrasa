3c79f37dc35a77dad815b7865d9358ceb2775612
Jacob Phillips <hello@jacobphillips.com>
2021-06-20 22:51:01 -0400

    Initial dev commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+* Start Bootstrap - Small Business v5.0.1 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+window.addEventListener('DOMContentLoaded', event => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl){return new bootstrap.Tooltip(tooltipTriggerEl)})
+    // Activate Bootstrap scrollspy on the main nav element
+    const topNav = document.body.querySelector('#topNav');
+    if (topNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#topNav',
+            offset: 80,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarSupportedContent .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
